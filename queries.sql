e0833dbecd710f701d95a307f9c1654e90090e93
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-26 18:36:37 +0300

    Delete, create savepoint and rollback

--- queries.sql
+++ queries.sql
@@ -29,3 +29,12 @@ DELETE FROM animals;
 SELECT * FROM animals; -- Verify that the changes were made
 ROLLBACK;
 SELECT * FROM animals; -- Verify that the changes were reverted
+
+BEGIN;
+DELETE  FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals; -- Verify that the changes were made
