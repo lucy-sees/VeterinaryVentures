67cc7ea948e1e3c7484b13c4158135a9f632283e
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-28 16:31:43 +0300

    Create vets, specialisation and visits tables

--- schema.sql
+++ schema.sql
@@ -53,3 +53,46 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_owners
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
+
+-- Create a table named vets with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+-- age: integer
+-- date_of_graduation: date
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+-- Create a table named specializations with the following columns:
+-- vet_id: integer
+-- species_id: integer
+-- Make sure that vet_id and species_id are set as a composite PRIMARY KEY
+-- Make sure that vet_id is a foreign key referencing vets table
+-- Make sure that species_id is a foreign key referencing species table
+CREATE TABLE specializations (
+    vet_id INT,
+    species_id INT,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id),
+    FOREIGN KEY (species_id) REFERENCES species(id)
+);
+
+-- Create a table named visits with the following columns:
+-- animal_id: integer
+-- vet_id: integer
+-- visit_date: date
+-- Make sure that animal_id, vet_id and visit_date are set as a composite PRIMARY KEY
+-- Make sure that animal_id is a foreign key referencing animals table
+-- Make sure that vet_id is a foreign key referencing vets table
+CREATE TABLE visits (
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date),
+    FOREIGN KEY (animal_id) REFERENCES animals(id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
